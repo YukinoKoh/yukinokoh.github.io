b9824d6f0574414e4c1cac2cc06f3e8bdd86036c
YukinoKohmoto <yukino@YukinoKohmotos-MacBook-Pro.local>
2017-05-21 02:27:37 +0900

    sorted styles following guidelines

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ $(window).scroll(function() {
   var height = $(document).scrollTop(); 
   if (height >= 30){
     $("#arrow").css("visibility","visible");
-    $("#arrow").addClass("animation_up");
+    $("#arrow").addClass("animation-up");
   }else{
     $("#arrow").css("visibility","hidden");
-    $("#arrow").removeClass("animation_up");
+    $("#arrow").removeClass("animation-up");
   }
 });
 
