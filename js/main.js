9c390b7d55cb79605249a5e5a89ca4e048af278b
YukinoKohmoto <yukino@YukinoKohmotos-MacBook-Pro.local>
2017-10-02 16:43:44 +0900

    removing sw

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-if('serviceWorker' in navigator) {
+/* if('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js').then(function(registration) {
     // Registration was successful
   }).catch(function(err) {
@@ -6,4 +6,11 @@ if('serviceWorker' in navigator) {
     console.log('ServiceWorker registration failed: ', err);
   });
 }
+*/
+
+navigator.serviceWorker.getRegistrations().then(function(registrations) {
+ for(let registration of registrations) {
+  registration.unregister()
+} })
+
 
