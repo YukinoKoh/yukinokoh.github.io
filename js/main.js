423c6b478f1077fedbe7c5d9d41d34aa864165a0
yukino <yukino@Ys.local>
2022-05-15 18:14:41 +0900

    letter hidden

--- js/main.js
+++ js/main.js
@@ -1,34 +1,24 @@
-function add_text() {
-  const header_text = `
-    `;
-
-  const footer_text = `
+const footer_text = `
     
     <div class="container clear">
-      <div class="s-full l-full">
+      <div class="full m1">
         <a href="index.html"><h3>Yukino Kohmoto</h3></a>
       </div>
       <div class="s-full l-col6">
-        <ul class="n-b-p">
+        <ul class="no-bullet">
           <li><a href="observe.html">Observe</a></li>
           <li><a href="imagine.html">Imagine</a></li>
           <li><a href="visualize.html">Visualize</a></li>
         </ul>
       </div>
-        <div class="s-full l-col6-r">
-          <ul class="n-b-p">
-            <li><span class="label">Email</span>
-              <a href="mailto:yukinokohmoto@gmail.com" target="_blank">yukino.kohmoto@gmail.com</a></li>
-            <li><span class="label">LinkedIn</span>
-              <a href="https://www.linkedin.com/in/yukino-komoto-a7345a19/" target="_blank">/yukino-komoto-a7345a19</a></li>
-            <li><span class="label">Instagram</span>
-              <a href="https://www.instagram.com/yukinokoh/" target="_blank">/yukinokoh</a></li>
-            <li><span class="label">GitHub</span>
-              <a href="https://github.com/YukinoKoh" target="_blank">/YukinoKoh</a></li>
+      <div class="s-full l-col4-r">
+          <ul class="no-bullet">
+            <li><a href="mailto:yukinokohmoto@gmail.com" target="_blank">Email</a></li>
+            <li><a href="https://www.linkedin.com/in/yukino-komoto-a7345a19/" target="_blank">linkedIn</a></li>
+            <li><a href="https://www.instagram.com/yukinokoh/" target="_blank">Instagram</a></li>
           </ul>
-        </div>
       </div>
-      <div class="container clear">
+      <div class="full m1 clear">
         <div class="mr-1">
           <p class="sub">This website is set with Clarika Pro from
             <a class="footer" href="https://www.behance.net/wildxedge" target="_blank">Brandon Knap</a>
@@ -37,6 +27,10 @@ function add_text() {
             This site tracks its stats with <a class="footer" href="https://marketingplatform.google.com/about/analytics/" target="_blank">Google Analytics</a>.</p>
         </div>
       </div>`  
+function add_text() {
+
+
+  
 
   // <document.getElementById("headerNavi").innerHTML = header_text;
   document.getElementById("footerNavi").innerHTML = footer_text;
