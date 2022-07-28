d541784d170ea492231bc9d8ea41e67df25fb227
yukino <yukino@Ys.local>
2022-07-28 11:07:53 +0900

    title animaion

---
+++ js/effect.js
@@ -0,0 +1,17 @@
+var Animation = function() {
+    
+    if(scrollY >650){
+        const title= document.getElementById('title-simeji').classList.add('show');}
+    if(scrollY <630){
+        const title= document.getElementById('title-simeji').classList.remove('show');}
+    if(scrollY >3400 ){
+        const title= document.getElementById('title-natural-light').classList.add('show');}
+    if(scrollY <3370 ){
+        const title= document.getElementById('title-natural-light').classList.remove('show');}
+    if(scrollY >5800){
+        const title= document.getElementById('title-hex').classList.add('show');}
+    if(scrollY <5770){
+        const title= document.getElementById('title-hex').classList.remove('show');}
+}
+window.addEventListener('scroll', Animation);
+
