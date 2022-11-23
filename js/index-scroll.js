59a45035f433356179e877125ae3b4507ed79131
yukino <yukino@Ys.local>
2022-11-24 08:55:04 +0900

    Tweek on the site description

--- js/index-scroll.js
+++ js/index-scroll.js
@@ -7,9 +7,9 @@ var Animation = function() {
         const title= document.getElementById('title-natural-light').classList.add('show');}
     if(scrollY <3370 ){
         const title= document.getElementById('title-natural-light').classList.remove('show');}
-    if(scrollY >5800){
+    if(scrollY >5700){
         const title= document.getElementById('title-hex').classList.add('show');}
-    if(scrollY <5770){
+    if(scrollY <5670){
         const title= document.getElementById('title-hex').classList.remove('show');}
 }
 window.addEventListener('scroll', Animation);
